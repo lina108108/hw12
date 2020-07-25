7c5ef87aaf447714cbff80a315b6ba645868f847
Liana Veldiaeva <lianali108@gmail.com>
2020-07-25 21:37:16 +0200

    tests changed

---
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -0,0 +1,33 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+    Book book = new Book(1, "Story1", 1000, "Lada");
+
+    @Test
+    void shouldMatchByNameIfExists() {
+        String textToFind = "Story1";
+        assertTrue(book.matches(textToFind));
+    }
+
+    @Test
+    void shouldMatchByNameIfNotExists() {
+        String textToFind = "Story2";
+        assertFalse(book.matches(textToFind));
+    }
+
+    @Test
+    void shouldMatchByAuthorIfExists() {
+        String textToFind = "Lada";
+        assertTrue(book.matches(textToFind));
+    }
+
+    @Test
+    void shouldMatchByAuthorIfNotExists() {
+        String textToFind = "Mari";
+        assertFalse(book.matches(textToFind));
+    }
+}
